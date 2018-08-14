49b91f7ffa893915518b92d2fb4c735123040a10
sharnajh <sharnajhossain@gmail.com>
2018-08-14 16:33:10 -0400

    Sign in, sign up MVP fixed

--- app.rb
+++ app.rb
@@ -12,8 +12,32 @@ get '/' do
     erb :index
 end
 
-post '/' do
-    @account = Account.create(
+get '/signin' do
+    erb :signin
+end
+
+post '/signin' do
+    @user = Account.find_by(username: params[:username])
+    if @user && @user.password == params[:password]
+        session[:user_id] = @user.id
+        @realname = @user.name.partition(" ").first
+    else
+        redirect "/"
+    end
+    erb :index
+end
+
+get "/signout" do
+    session[:user_id] = nil
+    redirect "/"
+  end
+
+get '/signup' do
+    erb :signup
+end
+
+post '/signup' do
+    @user = Account.create(
       username: params[:username],
       password: params[:password],
       name: params[:name],
