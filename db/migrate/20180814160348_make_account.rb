1d6b5be93849344ed147c7d3f2ebecb39b1f1f05
sharnajh <sharnajhossain@gmail.com>
2018-08-14 13:20:23 -0400

    Set up migrations

--- db/migrate/20180814160348_make_account.rb
+++ db/migrate/20180814160348_make_account.rb
@@ -1,4 +1,10 @@
 class MakeAccount < ActiveRecord::Migration[5.2]
   def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :email
+      t.string :username
+      t.string :password
+    end
   end
 end
