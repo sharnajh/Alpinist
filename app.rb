045cf0f063dd51dd10fa4ab36d3752b46c88a7a1
sharnajh <sharnajhossain@gmail.com>
2018-08-15 15:02:37 -0400

    Delete account option

--- app.rb
+++ app.rb
@@ -66,3 +66,27 @@ get '/post' do
         erb :signin
     end
 end
+
+post '/post' do
+    if session[:user_id]
+        @post = Post.create(account_id: session[:user_id], description: params[:text_body])
+        @user = Account.find_by(id: session[:user_id])
+        @user_name = "#{@user.first_name} #{@user.last_name}"
+        @textbody = @post.description
+    else
+        erb :signin
+    end
+end
+
+get '/settings' do
+    erb :settings
+end
+
+delete '/delete/:id' do
+    if session[:user_id]
+        @user = Account.delete(params[:id])
+        redirect to("/")
+    else 
+        redirect to("/settings")
+    end
+end
